ee04a616027ad69ded1bbd33983c5819ed6af63c
huy <huy@gmail.com>
2021-08-04 15:35:55 +0700

    hoan thanh app caculator

---
+++ app/src/main/java/com/example/caculatorapp/Constants.kt
@@ -0,0 +1,29 @@
+package com.example.caculatorapp
+
+object Constants {
+    object NUMBER {
+        const val NUMBER_ZERO = "0"
+        const val NUMBER_ONE = "1"
+        const val NUMBER_TWO = "2"
+        const val NUMBER_THREE = "3"
+        const val NUMBER_FOUR = "4"
+        const val NUMBER_FIVE = "5"
+        const val NUMBER_SIX = "6"
+        const val NUMBER_SEVEN = "7"
+        const val NUMBER_EIGHT = "8"
+        const val NUMBER_NINE = "9"
+        const val DOT = "."
+    }
+    object OPERATOR {
+        const val OPERATOR_PLUS = "+"
+        const val OPERATOR_SUB = "-"
+        const val OPERATOR_DIV = "/"
+        const val OPERATOR_MUL = "*"
+        const val OPERATOR_Percentage="%"
+    }
+
+    object DEFAULT {
+        const val DEFAULT_ZERO = "0"
+        const val DEFAULT_STRING_EMPTY = ""
+    }
+}
