1a1baa18cf51181ab4a44aed543f7025e4ade61d
Your Name <you@example.com>
2021-08-09 10:58:13 +0700

    sua het loi

--- app/src/main/java/com/example/caculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/caculatorapp/MainActivity.kt
@@ -10,142 +10,74 @@ import com.example.caculatorapp.databinding.ActivityMainBinding
 import java.util.*
 import kotlin.collections.ArrayList
 
-<<<<<<< HEAD
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-
-    private lateinit var binding: ActivityMainBinding
-    private var buttons = listOf<Button>()
-    private var textExpression = Constants.DEFAULT.DEFAULT_STRING_EMPTY
-
-=======
-
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
     private var buttons = listOf<Button>()
     private var textExpression = Constants.DEFAULT.DEFAULT_STRING_EMPTY
->>>>>>> ee04a61 (hoan thanh app caculator)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setItemOnClicks()
     }
-<<<<<<< HEAD
 
-    private fun setItemOnClicks(){
+    private fun setItemOnClicks() {
         with(binding) {
             buttons = listOf(
-                buttonZero,
-                buttonOne,
-                buttonTwo,
-                buttonThree,
-                buttonFour,
-                buttonFive,
-                buttonSix,
-                buttonSeven,
-                buttonEight,
-                buttonNine,
-                buttonDot,
-                buttonDivision,
-                buttonAdd,
-                buttonMultiply,
-                buttonSubtract,
-                buttonCE,
-                buttonDelete,
-                buttonBracketOpen,
-                buttonBracketClose,
-                buttonSolve
-=======
-    private fun setItemOnClicks(){
-        with(binding) {
-            buttons = listOf(
-                btnZero,
-                btnOne,
-                btnTwo,
-                btnThree,
-                btnFour,
-                btnFive,
-                btnSix,
-                btnSeven,
-                btnEight,
-                btnNine,
-                btnDot,
-                btnDiv,
-                btnAdd,
-                btnMul,
-                btnSub,
-                btnClear,
-                btnDelete,
-                btnPercent,
-                btnResult
->>>>>>> ee04a61 (hoan thanh app caculator)
-            )
-        }
-        buttons.forEach { it.setOnClickListener(this) }
-    }
-<<<<<<< HEAD
-
-    override fun onClick(v: View?)= with(binding) {
-        when (v) {
-            buttonDot -> addTextCalculator(Constants.NUMBER.DOT)
-            buttonZero -> addTextCalculator(Constants.NUMBER.NUMBER_ZERO)
-            buttonOne -> addTextCalculator(Constants.NUMBER.NUMBER_ONE)
-            buttonTwo -> addTextCalculator(Constants.NUMBER.NUMBER_TWO)
-            buttonThree -> addTextCalculator(Constants.NUMBER.NUMBER_THREE)
-            buttonFour -> addTextCalculator(Constants.NUMBER.NUMBER_FOUR)
-            buttonFive -> addTextCalculator(Constants.NUMBER.NUMBER_FIVE)
-            buttonSix -> addTextCalculator(Constants.NUMBER.NUMBER_SIX)
-            buttonSeven -> addTextCalculator(Constants.NUMBER.NUMBER_SEVEN)
-            buttonEight -> addTextCalculator(Constants.NUMBER.NUMBER_EIGHT)
-            buttonNine -> addTextCalculator(Constants.NUMBER.NUMBER_NINE)
-            buttonDivision -> addTextCalculator(Constants.OPERATOR.OPERATOR_DIV)
-            buttonSubtract -> addTextCalculator(Constants.OPERATOR.OPERATOR_SUB)
-            buttonAdd -> addTextCalculator(Constants.OPERATOR.OPERATOR_PLUS)
-            buttonMultiply -> addTextCalculator(Constants.OPERATOR.OPERATOR_MUL)
-            buttonBracketClose->addTextCalculator(Constants.OPERATOR.BRACKET_CLOSE)
-            buttonBracketOpen->addTextCalculator(Constants.OPERATOR.BRACKET_OPEN)
-            buttonCE -> clearScreen()
-            buttonDelete -> delete()
-            buttonSolve -> {
-                val arrayPrefix = convertPrefix(textExpression)
-                val result = calculate(arrayPrefix).toString()
-                binding.tvResult.text = result
-=======
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.btn_dot -> addTextCalculator(Constants.NUMBER.DOT)
-            R.id.btn_zero -> addTextCalculator(Constants.NUMBER.NUMBER_ZERO)
-            R.id.btn_one -> addTextCalculator(Constants.NUMBER.NUMBER_ONE)
-            R.id.btn_two -> addTextCalculator(Constants.NUMBER.NUMBER_TWO)
-            R.id.btn_three -> addTextCalculator(Constants.NUMBER.NUMBER_THREE)
-            R.id.btn_four -> addTextCalculator(Constants.NUMBER.NUMBER_FOUR)
-            R.id.btn_five -> addTextCalculator(Constants.NUMBER.NUMBER_FIVE)
-            R.id.btn_six -> addTextCalculator(Constants.NUMBER.NUMBER_SIX)
-            R.id.btn_seven -> addTextCalculator(Constants.NUMBER.NUMBER_SEVEN)
-            R.id.btn_eight -> addTextCalculator(Constants.NUMBER.NUMBER_EIGHT)
-            R.id.btn_nine -> addTextCalculator(Constants.NUMBER.NUMBER_NINE)
-            R.id.btn_div -> addTextCalculator(Constants.OPERATOR.OPERATOR_DIV)
-            R.id.btn_sub -> addTextCalculator(Constants.OPERATOR.OPERATOR_SUB)
-            R.id.btn_add -> addTextCalculator(Constants.OPERATOR.OPERATOR_PLUS)
-            R.id.btn_mul -> addTextCalculator(Constants.OPERATOR.OPERATOR_MUL)
-            R.id.btn_percent -> addTextCalculator(Constants.OPERATOR.OPERATOR_Percentage)
-            R.id.btn_clear -> {
-                clearScreen()
-            }
-            R.id.btn_delete -> {
-                delete()
-            }
-            R.id.btn_Result -> {
-                val arrayPrefix = convertPrefix(textExpression)
-                val result = calculate(arrayPrefix).toString()
-                binding.txtResult.text = result
->>>>>>> ee04a61 (hoan thanh app caculator)
-                textExpression = Constants.DEFAULT.DEFAULT_STRING_EMPTY
-            }
-
+                    buttonZero,
+                    buttonOne,
+                    buttonTwo,
+                    buttonThree,
+                    buttonFour,
+                    buttonFive,
+                    buttonSix,
+                    buttonSeven,
+                    buttonEight,
+                    buttonNine,
+                    buttonDot,
+                    buttonDivision,
+                    buttonAdd,
+                    buttonMultiply,
+                    buttonSubtract,
+                    buttonCE,
+                    buttonDelete,
+                    buttonBracketOpen,
+                    buttonBracketClose,
+                    buttonSolve)
+            buttons.forEach { it.setOnClickListener(this@MainActivity) }
         }
     }
-<<<<<<< HEAD
+    override fun onClick(view: View?) = with(binding) {
+            when (view) {
+                buttonDot -> addTextCalculator(Constants.NUMBER.DOT)
+                buttonZero -> addTextCalculator(Constants.NUMBER.NUMBER_ZERO)
+                buttonOne -> addTextCalculator(Constants.NUMBER.NUMBER_ONE)
+                buttonTwo -> addTextCalculator(Constants.NUMBER.NUMBER_TWO)
+                buttonThree -> addTextCalculator(Constants.NUMBER.NUMBER_THREE)
+                buttonFour -> addTextCalculator(Constants.NUMBER.NUMBER_FOUR)
+                buttonFive -> addTextCalculator(Constants.NUMBER.NUMBER_FIVE)
+                buttonSix -> addTextCalculator(Constants.NUMBER.NUMBER_SIX)
+                buttonSeven -> addTextCalculator(Constants.NUMBER.NUMBER_SEVEN)
+                buttonEight -> addTextCalculator(Constants.NUMBER.NUMBER_EIGHT)
+                buttonNine -> addTextCalculator(Constants.NUMBER.NUMBER_NINE)
+                buttonDivision -> addTextCalculator(Constants.OPERATOR.OPERATOR_DIV)
+                buttonSubtract -> addTextCalculator(Constants.OPERATOR.OPERATOR_SUB)
+                buttonAdd -> addTextCalculator(Constants.OPERATOR.OPERATOR_PLUS)
+                buttonMultiply -> addTextCalculator(Constants.OPERATOR.OPERATOR_MUL)
+                buttonBracketClose -> addTextCalculator(Constants.OPERATOR.BRACKET_CLOSE)
+                buttonBracketOpen -> addTextCalculator(Constants.OPERATOR.BRACKET_OPEN)
+                buttonCE -> clearScreen()
+                buttonDelete -> delete()
+                buttonSolve -> {
+                    val arrayPrefix = convertPrefix(textExpression)
+                    val result = calculate(arrayPrefix).toString()
+                    binding.tvResult.text = result
+                    textExpression = Constants.DEFAULT.DEFAULT_STRING_EMPTY
+                }
+
+            }
+        }
 
     private fun clearScreen() {
         binding.tvCalculator.text = Constants.DEFAULT.DEFAULT_ZERO
@@ -155,58 +87,26 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun delete() {
         textExpression =textExpression.substring(0,textExpression.length-Constants.DEFAULT.DEFAULT_NUMBER_ONE)
-        binding.tvCalculator.text=textExpression.toEditable()
+        binding.tvCalculator.text=textExpression
     }
 
-    private fun addTextCalculator(text: String){
-            textExpression += text;
-            binding.tvCalculator.text = textExpression.toEditable()
+    private fun addTextCalculator(text: String) {
+        textExpression += text
+        binding.tvCalculator.text = textExpression
     }
 
-    private fun priority(operator: String):Int{
-        return when(operator){
+    private fun priority(operator: String) =  when(operator) {
             Constants.OPERATOR.OPERATOR_PLUS, Constants.OPERATOR.OPERATOR_SUB -> Constants.PRIORITY.PRIORITY_ONE
             Constants.OPERATOR.OPERATOR_DIV, Constants.OPERATOR.OPERATOR_MUL -> Constants.PRIORITY.PRIORITY_TWO
             else -> Constants.PRIORITY.PRIORITY_DEFAULT
-        }
     }
 
-=======
-    private fun clearScreen() {
-        binding.txtExpression.text = Constants.DEFAULT.DEFAULT_ZERO.toEditable()
-        binding.txtResult.text = Constants.DEFAULT.DEFAULT_ZERO
-        textExpression= Constants.DEFAULT.DEFAULT_STRING_EMPTY
-        binding.txtExpression.text = textExpression.toEditable()
-        binding.txtExpression.setSelection(binding.txtExpression.length());
-    }
-    private fun delete() {
-        textExpression=binding.txtExpression.text.toString()
-        if(textExpression.isNotEmpty()){
-            binding.txtExpression.text=textExpression.substring(0, textExpression.length - 1).toEditable()
-            textExpression=textExpression.substring(0, textExpression.length - 1)
-        }
-
-    }
-    private fun addTextCalculator(text: String){
-            textExpression += text;
-            binding.txtExpression.text = textExpression.toEditable()
-            binding.txtExpression.setSelection(binding.txtExpression.length());
-
-    }
-    private fun priority(operator: String):Int{
-        return when(operator){
-            Constants.OPERATOR.OPERATOR_PLUS, Constants.OPERATOR.OPERATOR_SUB -> 1
-            Constants.OPERATOR.OPERATOR_DIV, Constants.OPERATOR.OPERATOR_MUL, Constants.OPERATOR.OPERATOR_Percentage -> 2
-            else -> -1
-        }
-    }
->>>>>>> ee04a61 (hoan thanh app caculator)
-    private fun checkOperator(text: String): Boolean{
-        if(text == Constants.OPERATOR.OPERATOR_PLUS ||
-            text == Constants.OPERATOR.OPERATOR_MUL ||
-            text == Constants.OPERATOR.OPERATOR_SUB ||
-<<<<<<< HEAD
-            text == Constants.OPERATOR.OPERATOR_DIV) return true
+    private fun checkOperator(text: String): Boolean {
+        if (text == Constants.OPERATOR.OPERATOR_PLUS ||
+                text == Constants.OPERATOR.OPERATOR_MUL ||
+                text == Constants.OPERATOR.OPERATOR_SUB ||
+                text == Constants.OPERATOR.OPERATOR_DIV
+        ) return true
         return false
     }
 
@@ -214,16 +114,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val stackNumber = Stack<Double>()
         for (i in arrayPrefix) {
             val num = i.toDoubleOrNull()
-=======
-            text == Constants.OPERATOR.OPERATOR_DIV||
-            text == Constants.OPERATOR.OPERATOR_Percentage) return true
-        return false
-    }
-    private fun calculate(arrayPrefix: ArrayList<String>): Double {
-        val stackNumber = Stack<Double>()
-        for (i in arrayPrefix) {
-            val num: Double? = i.toDoubleOrNull()
->>>>>>> ee04a61 (hoan thanh app caculator)
             if (num != null) stackNumber.push(num)
             else {
                 val number1 = stackNumber.pop().toDouble()
@@ -233,66 +123,51 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                     Constants.OPERATOR.OPERATOR_SUB -> stackNumber.push(number2 - number1)
                     Constants.OPERATOR.OPERATOR_DIV -> stackNumber.push(number2 / number1)
                     Constants.OPERATOR.OPERATOR_MUL -> stackNumber.push(number2 * number1)
-<<<<<<< HEAD
-=======
-                    Constants.OPERATOR.OPERATOR_Percentage -> stackNumber.push(number2 % number1)
->>>>>>> ee04a61 (hoan thanh app caculator)
                 }
             }
 
         }
-<<<<<<< HEAD
         return if (stackNumber.isNotEmpty()) stackNumber.pop().toDouble() else Constants.DEFAULT.DEFAULT_DOUBLE_ZERO
     }
 
-=======
-        return if (!stackNumber.isEmpty()) stackNumber.pop().toDouble() else 0.0
-    }
->>>>>>> ee04a61 (hoan thanh app caculator)
-    private fun convertPrefix(token: String): ArrayList<String> {
+    private fun convertPrefix(text: String): ArrayList<String> {
         val arrayPrefix = ArrayList<String>()
         val stackOperator = Stack<String>()
-        var num = " "
-        for(i in token.indices){
-            if (!checkOperator(token[i].toString())){
-                num += token[i]
-                if(i == token.length - 1){
+        var num = Constants.DEFAULT.DEFAULT_STRING_EMPTY
+        for (i in text.indices) {
+            if (!checkOperator(text[i].toString())) {
+                num += text[i]
+                if (i == text.length - Constants.DEFAULT.DEFAULT_NUMBER_ONE) {
                     arrayPrefix.add(num)
                     num = Constants.DEFAULT.DEFAULT_STRING_EMPTY
                 }
-            }else{
+            } else {
                 arrayPrefix.add(num)
-<<<<<<< HEAD
-                num =Constants.DEFAULT.DEFAULT_STRING_EMPTY
-                while(stackOperator.isNotEmpty() && priority(token[i].toString()) <= priority(
-=======
-                num =" "
-                while(!stackOperator.isEmpty() && priority(token[i].toString()) <= priority(
->>>>>>> ee04a61 (hoan thanh app caculator)
-                        stackOperator.peek()
-                    )){
+                num = Constants.DEFAULT.DEFAULT_STRING_EMPTY
+                while (!stackOperator.isEmpty() && priority(text[i].toString()) <= priority(
+                                stackOperator.peek()
+                        )
+                ) {
                     arrayPrefix.add(stackOperator.pop())
                 }
-                stackOperator.push(token[i].toString())
+                stackOperator.push(text[i].toString())
             }
         }
-<<<<<<< HEAD
-        while(stackOperator.isNotEmpty()){
-=======
-        while(!stackOperator.isEmpty()){
->>>>>>> ee04a61 (hoan thanh app caculator)
+        while (stackOperator.isNotEmpty()) {
             arrayPrefix.add(stackOperator.pop())
         }
-        if(num.isNotEmpty()){
+        if (num.isNotEmpty()) {
             arrayPrefix.add(num)
         }
         return arrayPrefix
     }
-
-    fun String.toEditable(): Editable =  Editable.Factory.getInstance().newEditable(this)
-
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> ee04a61 (hoan thanh app caculator)
+
+
+
+
+
+
+
+
+
